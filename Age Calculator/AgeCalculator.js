ada8e0e265b5fd04838fdb6f5f2fa806d7d62b47
i300i <50527186+i300i@users.noreply.github.com>
2019-05-14 16:45:20 +0200

    Add files via upload

---
+++ Age Calculator/AgeCalculator.js
@@ -0,0 +1,98 @@
+var button = document.querySelector("button");
+var messageDisplay = document.querySelector(".resultMessage");
+var numberDisplay = document.querySelector("#number");
+
+button.addEventListener ("click", function calculatedResult(){
+	var result = Math.round($("input").val() * 365.2);
+	console.log(result);
+
+	 $(".resultMessage").text("You've been alive for " + result + " days").fadeIn();
+});
+
+//Reset
+
+resetButton.addEventListener("click", function reset(){
+	$(".resultMessage").text("").fadeOut();
+	numberDisplay.textContent = "";
+});
+
+//Block typing letters:
+
+$(document).on("keyup" , "#regNo" , function() {
+	if(this.value && isNaN(this.value)) {
+
+} });
+
+//Block typing negative numbers:
+
+var number = document.getElementById('number');
+number.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+// Background:
+
+var colors = new Array(
+  [62,35,255],
+  [60,255,60],
+  [255,35,98],
+  [45,175,230],
+  [255,0,255],
+  [255,128,0]);
+
+var step = 0;
+var colorIndices = [0,1,2,3];
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
+  if ( $===undefined ) return;
+  
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
+
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+ $('body').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+  
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    
+  }
+}
+
+setInterval(updateGradient,10);
+
+// 'Enter' key:
+
+$("#number").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#fuckingButton").click();
+    }
+});
